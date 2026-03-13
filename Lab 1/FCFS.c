6816c3328d4081752f8d68ddcf3f9958d179612d
Ranjith282006 <ranjithr.cs25@bmsce.ac.in>
2026-03-13 12:17:18 +0530

    Add files via upload

---
+++ Lab 1/FCFS.c
@@ -0,0 +1,50 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("Name:Ranjith R\n" "USN:1BM25CS501\n" "PRG:FCFS\n");
+    int n, i;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    int AT[n], BT[n], CT[n], TAT[n], WT[n], RT[n];
+    int current_time = 0;
+
+    float avgTAT = 0, avgWT = 0;
+
+    for(i = 0; i < n; i++) {
+        printf("Enter Arrival Time and Burst Time for P%d: ", i+1);
+        scanf("%d %d", &AT[i], &BT[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(current_time < AT[i])
+            current_time = AT[i];
+
+        RT[i] = current_time - AT[i];
+
+        CT[i] = current_time + BT[i];
+        TAT[i] = CT[i] - AT[i];
+        WT[i] = TAT[i] - BT[i];
+
+        current_time = CT[i];
+
+        avgTAT += TAT[i];
+        avgWT += WT[i];
+    }
+
+   printf("\n%-5s %-12s %-10s %-15s %-15s %-12s %-12s\n",
+    "P","ArrivalTime","BurstTime","CompletionTime","TurnAroundTime","WaitingTime","ResponseTime");
+
+    for(i=0;i<n;i++)
+        printf("%-5d %-12d %-10d %-15d %-15d %-12d %-12d\n",
+        i+1, AT[i], BT[i], CT[i], TAT[i], WT[i], RT[i]);
+
+
+    printf("\nAverage TurnAroundTime = %.2f",avgTAT/n);
+    printf("\nAverage WaitingTime = %.2f",avgWT/n);
+
+
+    return 0;
+}
+
---
+++ Lab 1/SRTF.C
@@ -0,0 +1,67 @@
+
+#include <stdio.h>
+
+int main() {
+    printf("Name:Ranjith R\n" "USN:1BM25CS501\n""PRG:SRTF\n");
+    int n;
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    int AT[n], BT[n], RT[n], CT[n], TAT[n], WT[n], start[n];
+    int i, time = 0, completed = 0, min, pos;
+
+    float avgTAT=0, avgWT=0;
+
+    for(i=0;i<n;i++){
+        printf("Enter AT and BT for P%d: ",i+1);
+        scanf("%d %d",&AT[i],&BT[i]);
+
+        RT[i] = BT[i];
+        start[i] = -1;
+    }
+
+    while(completed < n){
+        min = 999;
+
+        for(i=0;i<n;i++){
+            if(AT[i] <= time && RT[i] > 0 && RT[i] < min){
+                min = RT[i];
+                pos = i;
+            }
+        }
+
+        if(min == 999){
+            time++;
+        }
+        else{
+            if(start[pos] == -1)
+                start[pos] = time;
+
+            RT[pos]--;
+            time++;
+
+            if(RT[pos] == 0){
+                completed++;
+                CT[pos] = time;
+                TAT[pos] = CT[pos] - AT[pos];
+                WT[pos] = TAT[pos] - BT[pos];
+
+                avgTAT += TAT[pos];
+                avgWT += WT[pos];
+            }
+        }
+    }
+
+    printf("\n%-5s %-12s %-10s %-15s %-15s %-12s %-12s\n",
+    "P","ArrivalTime","BurstTime","CompletionTime","TurnAroundTime","WaitingTime","ResponseTime");
+
+    for(i=0;i<n;i++)
+        printf("%-5d %-12d %-10d %-15d %-15d %-12d %-12d\n",
+        i+1, AT[i], BT[i], CT[i], TAT[i], WT[i], start[i]-AT[i]);
+
+    printf("\nAverage TurnAroundTime = %.2f",avgTAT/n);
+    printf("\nAverage WaitingTime = %.2f",avgWT/n);
+
+    return 0;
+}
+
